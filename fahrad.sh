20d26ea0cbceba35017cf4b97aec36ebbaf5b973
Heinz-Wilhelm Schäbe <heinz-wilhelm@schaebe.com>
2015-06-26 23:53:06 +0200

    Kleinigkeiten

--- fahrad.sh
+++ fahrad.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
-read -p "Willkommen bei U-Ride. Bitte wählen!" ausw #>Display
+#Varialben
+ausw=0
+DATE=0
+BNUM=0
+PIN=0
+read -p "Willkommen bei U-Ride. Bitte Benutzernummer eingeben!" BNUM #>Display
+# TODO MySQL Abfrage
+if [[ $BNUM == 0 ]]; then
+  #statements
+else
+
+fi
 if [[ $ausw == 1 ]]; then
   #AUSLEIHE
   #Überprüfung Uhrzeit -> Variable
-    	DATE=$(date +%H)
-      if [[  $DATE  == 10 || $DATE  == 11 || $DATE  == 12 || $DATE  == 13 || $DATE  == 14 || $DATE  == 15 || $DATE  == 16 || $DATE  == 17 || $DATE  == 18 || $DATE  == 19 || $DATE  == 20 || $DATE  == 21 ]]; then
+    	DATEH=$(date +%H)
+      DATED=$(date +%u )
+      if [[  $DATEH  == 10 || $DATEH  == 11 || $DATEH  == 12 || $DATEH  == 13 || $DATEH  == 14 || $DATEH  == 15 || $DATEH  == 16 || $DATEH  == 17 || $DATEH  == 18 || $DATEH  == 19 || $DATEH  == 20 || $DATEH  == 21  ]]; then
+      #TODO Wochentag implementieren und Abfrage
         #AUSLEIHE ok
       else
         #ausleihe verweigern
-        
+
       fi
       #TODO Uhrzeit interpretieren, ggf. keine ausleihe.
       #TODO Hintertür
